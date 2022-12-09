0e3a32131956edb1818293dad0aee4c76b7c8422
Silvan Kohler <silvankohler@protonmail.com>
2022-12-09 21:01:32 +0000

    install log2ram via apt

--- stage3/00-install-log2ram/00-run.sh
+++ stage3/00-install-log2ram/00-run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
 on_chroot <<EOF
-git clone https://github.com/azlux/log2ram.git
-./log2ram/install.sh
-rm -rf ./log2ram/
+echo "deb [signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian/ bullseye main" >> /etc/apt/sources.list.d/azlux.list
+wget -O /usr/share/keyrings/azlux-archive-keyring.gpg  https://azlux.fr/repo.gpg
+apt-get update
+apt-get install -y --no-install-recommends log2ram
 
 sed -r 's/(SIZE*=).*/\1100M/' /etc/log2ram.conf > /etc/log2ram.conf
 sed -r 's/(ZL2R*=).*/\1true/' /etc/log2ram.conf > /etc/log2ram.conf
